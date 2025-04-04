7b3b217bd9de1f6972baf8ccd5aa0f5d7416ee47
Skoystah <geert.dooms@gmail.com>
2025-04-04 13:13:45 +0200

    fixed dirty code

--- src/text_to_node.py
+++ src/text_to_node.py
@@ -113,21 +113,10 @@ def split_nodes_link(old_nodes):
 
 
 def text_to_textnodes(text):
-    text_node = TextNode(text, TextType.TEXT)
-    nodes = split_nodes_link(
-            split_nodes_image(
-                split_nodes_delimiter(
-                    split_nodes_delimiter(
-                        split_nodes_delimiter(
-                            split_nodes_delimiter([text_node], "**", TextType.BOLD),
-                            "_",
-                            TextType.ITALIC),
-                        "*",
-                        TextType.ITALIC,
-                        ),
-                    "`",
-                    TextType.CODE,
-                    )
-                )
-    )
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
     return nodes
